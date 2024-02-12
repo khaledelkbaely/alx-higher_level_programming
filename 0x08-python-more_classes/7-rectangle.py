f2d9c627829f9d22c6647d050f2ba64381b02b85
khaled <empeshy20036969@gmail.com>
2024-02-12 18:28:36 +0200

    8th

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -5,6 +5,7 @@
 class Rectangle:
     """Class for rectangle."""
     number_of_instances = 0
+    print_symbol = '#'
 
     def __init__(self, width=0, height=0):
         """
@@ -97,7 +98,7 @@ class Rectangle:
 
         for i in range(self.__height):
             for j in range(self.__width):
-                s += '#'
+                s += '{}'.format(self.print_symbol)
             if i != self.__height - 1:
                 s += '\n'
         return s
