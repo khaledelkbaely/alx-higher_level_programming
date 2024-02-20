43466f41d70ec1dc80418fac3b5caec7b32253ef
khaled <empeshy20036969@gmail.com>
2024-02-20 21:23:55 +0200

    2nd

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,7 +3,9 @@
 
 
 class MyList(list):
+    """MyList class"""
     def print_sorted(self):
+        """function to print sorted list without changing original"""
         copy = self[:]
         copy.sort()
         print(copy)
