412afaf6848543bc4bdf9ad2bf754553b19d8ad6
khaled <empeshy20036969@gmail.com>
2024-02-20 22:52:21 +0200

    9th

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -23,3 +23,12 @@ class BaseGeometry:
             raise TypeError('{} must be an integer'.format(name))
         if value <= 0:
             raise ValueError('{} must be greater than 0'.format(name))
+
+class Rectangle(BaseGeometry):
+    """class for rectangle"""
+    def __init__(self, width, height):
+        """constructor."""
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
+        self.__width = width
+        self.__height = height
