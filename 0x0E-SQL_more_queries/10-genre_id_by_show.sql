70e06d162ef823d14f6528e9d3411532ad799163
Khaled Empeshy <empeshy20036969@gmail.com>
2024-03-28 22:36:45 +0200

    fix

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,6 +2,6 @@
 -- that have at least one genre linked
 SELECT tvs.title AS title, tvsg.genre_id AS genre_id
 FROM tv_shows AS tvs
-INNER JOIN tv_show_genres as tvsg
+INNER JOIN tv_show_genres AS tvsg
 ON tvs.id = tvsg.show_id
 ORDER BY title, genre_id;
