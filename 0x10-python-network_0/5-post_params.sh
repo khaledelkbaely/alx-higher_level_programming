aea78ed957656359ffce44813b160a4d658b5f1a
Khaled Empeshy <empeshy20036969@gmail.com>
2024-05-14 16:22:48 +0300

    5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send a request to url and displays the size of the body of the response
-curl -sI "$1" | grep "Allow" | cut -d ' ' -f2-
+curl -d 'email=test@gmail.com&subject=I will always be here for PLD' -s "$1"
