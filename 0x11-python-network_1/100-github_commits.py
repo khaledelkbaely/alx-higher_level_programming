591bc46a49f1dfa534043736fd88795c39c630de
Khaled Empeshy <empeshy20036969@gmail.com>
2024-05-16 11:20:00 +0300

    last

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" script thst fetches https://alx-intrannet.hbtn.io/status """
+import requests
+from sys import argv
+from pprint import pprint
+
+if __name__ == "__main__":
+    repo = argv[1]
+    owner = argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        repo, owner)
+
+    with requests.get(url) as response:
+        recent_ten_commits = response.json()[:11]
+        for commit in recent_ten_commits:
+            print("{}: {}".format(
+                  commit.get('sha'), commit.get(
+                      'commit').get('author').get('name')))
