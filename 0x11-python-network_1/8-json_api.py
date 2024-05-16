4e2b567d3f22d48929398ed58c154027f82be118
Khaled Empeshy <empeshy20036969@gmail.com>
2024-05-16 10:18:22 +0300

    8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" script thst fetches https://alx-intrannet.hbtn.io/status """
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    print(len(argv))
+    letter = argv[1] if len(argv) > 1 else ""
+
+    with requests.post(url, data={"q": letter}) as response:
+        try:
+            json_response = response.json()
+            if json_response:
+                print("[{}] {}".format(
+                      json_response.get('id'), json_response.get('name')))
+            else:
+                print("No result")
+        except Exception:
+            print("Not a valid JSON")
