387bfbef75759ab545f7b017381ec4c2f4f4c0f3
Khaled Empeshy <empeshy20036969@gmail.com>
2024-05-16 07:43:29 +0300

    fix 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,8 @@ if __name__ == "__main__":
 
     with urlopen(url) as response:
         body = response.read()
-        print("Body responce:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode('utf-8')))
+        body_utf8 = body.decode('utf-8')
+    print("Body responce:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body_utf8))
