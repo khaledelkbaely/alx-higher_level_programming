d077a47904d0df720646903b6960c603038e6a16
Khaled Empeshy <empeshy20036969@gmail.com>
2024-06-12 04:49:42 +0300

    8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+let n = parseInt(process.argv[2])
+let ch = 'X'
+if (Number.isNaN(n)) {
+  console.log('Missing size')
+  process.exit()
+}
+for (let i = 0; i < n; i++) {
+  let c = ch
+  for (let i = 1; i < n; i++) {
+    c += ch
+  }
+  console.log(c)
+}
