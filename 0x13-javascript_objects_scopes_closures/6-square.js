6ff1b027dcf1580670e1a0e2b51b896f78da0b6d
Khaled Empeshy <empeshy20036969@gmail.com>
2024-06-22 12:21:44 +0300

    fix6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle.js');
+const Sq = require('./5-square');
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+class Square extends Sq {
   charPrint (c) {
     const chP = c || 'X';
     let ln = '';
